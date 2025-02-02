6eb8548ebad01ec8f8416e75a2ae3025f3120ade
Aman Sharma <amanazr2003@gmail.com>
2025-02-02 20:48:00 +0530

    Contact Card Set Up

--- src/Components/ContactCard.jsx
+++ src/Components/ContactCard.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import { Firestore } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import {db} from '../config/Firebase';
 
 const ContactCard = () => {
+
+  let [contact, setContact]= useState([]);
+
+  useEffect (()=>{
+    let getContact= async()=>{
+      try {
+
+        let contactData= collection(db, "contact");
+        let dataSnapShot= await getDocs(contactData);
+        let outputData= dataSnapShot.docs.map((doc)=>{
+          return{
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContact(outputData);
+
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+
+    getContact();
+  },[])
+
   return (
     <>
-    <div>app</div>
+    <div className=''>
+      <div className='flex justify-center flex-col mt-2 '>
+        {
+          contact.map((contact)=>(
+            <div key={contact.id} className='w-[360px] flex bg-yellow-100 rounded-md mt-2 m-auto'>
+
+              <div className='w-10 text-[30px] flex justify-center text-orange-400'> <i class="ri-user-fill"> </i></div>
+
+              <div className='w-[240px]'>
+                <h2 className='font-medium pt-[2px]'>{contact.Name}</h2>
+                <p className='text-[14px] leading-1'>{contact.Email}</p>
+              </div>
+
+              <div className='w-[80px] flex justify-end gap-4 pt-[2px] cursor-pointer'>
+                <div className='text-[25px]'><i class="ri-edit-box-line"></i></div>
+                <div className='text-[25px] pr-1 text-purple-700 cursor-pointer'><i class="ri-delete-bin-7-fill"></i></div>
+              </div>
+
+            </div>
+          ))
+        }
+      </div>
+    </div>
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
     <div className='flex justify-center'>
 
-        <div className='flex gap-2 h-12 w-[350px] bg-white justify-center text-center rounded-md mt-2 pt-2'>
+        <div className='flex gap-2 h-12 w-[360px] bg-white justify-center text-center rounded-md mt-2 pt-2'>
           <div><img src= {Logo} alt="" /></div>
           <div className='font-medium text-[18px]'>Firebase contact App</div>
         </div>
