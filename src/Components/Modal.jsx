5acf8bf77bdec38b095cdddf57060095a05d38ac
Aman Sharma <amanazr2003@gmail.com>
2025-02-03 15:28:12 +0530

    modal box name and email input added

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { createPortal } from 'react-dom';
+import {Formik, Field, Form} from 'formik';
 
-const Modal = ({open, setOpen}) => {
+const Modal = ({open, setOpen, children}) => {
     let onClose =()=>{
         setOpen(false);
     }
@@ -11,10 +12,31 @@ const Modal = ({open, setOpen}) => {
         open && (
             <>
                 <div className='flex justify-center'>
-                    <div className='w-[300px] h-[200px] bg-white rounded-md relative z-995'>
+                    <div className='w-[300px] h-[210px] bg-white rounded-md relative z-995'>
 
                         <div className='flex justify-end text-[20px] pr-1'>
                             <i class="ri-close-large-fill cursor-pointer" onClick={onClose}></i>
+                        </div>
+
+                        <div>
+                            <Formik>
+                                <Form>
+                                    <div>
+                                        <div className='flex flex-col pl-3'>
+                                            <label htmlFor="Nmae" className='font-medium'>Name</label>
+                                            <Field Name="Name" placeholder='Enter Name' className='border outline-none w-[250px] h-8 rounded-md pl-1.5' />
+                                        </div>
+                                        <div className='flex flex-col pl-3 mt-1.5'>
+                                            <label htmlFor="Email" className='font-medium'>Email</label>
+                                            <Field Name="Email" placeholder='Enter Email' className='border outline-none w-[250px] h-8 rounded-md pl-1.5' />
+                                        </div>
+
+                                        <div className='flex ml-4 mt-3'>
+                                            <button className='bg-blue-500 w-30 h-9 rounded-md font-medium text-white cursor-pointer'>Add Contact</button>
+                                        </div>
+                                    </div>
+                                </Form>
+                            </Formik>
                         </div>
 
                     </div>
